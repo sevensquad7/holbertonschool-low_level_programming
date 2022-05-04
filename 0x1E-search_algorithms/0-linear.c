97610cc3c90e8ed5fddee86f4ba6d42c6f4aef73
zye7ert <edgarxot@gmail.com>
2022-05-04 13:53:55 -0500

    load file restructed tabs and correct iterator

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,6 +1,7 @@
 #include "search_algos.h"
+
 /**
- * function that searches for a value in an array of integers
+ * linear_search - function that searches for a value in an array of integers
  * using the Linear search algorithm
  * @array: in array task
  * @size: size of the array
@@ -10,19 +11,23 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
-    if (i >= size)
-        {
-            return -1;
-        }
-    for (i = 0; i < size; i++)
-    {
-        if (array[i] == value)
-        {
-            printf("value checked array[%ld] = [%i]\n", i, value);
-            return i;
-        }        
-    }
-    return -1;
+	size_t i = 0;
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] != value)
+		{
+			printf("value checked array[%ld] = [%i]\n", i, array[i]);
+		}
+		if (array[i] == value)
+		{
+			printf("value checked array[%ld] = [%i]\n", i, array[i]);
+			return (i);
+		}
+	}
+	return (-1);
 }
